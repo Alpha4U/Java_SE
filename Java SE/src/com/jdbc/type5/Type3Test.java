3eb2843bbccba8b705618e2c22f838a8a821482f
Shubham Awasthi <shubham.ethical@gmail.com>
2020-09-17 20:14:40 +0530

    JDBC Type 3 Type 5 Driver

---
+++ Java SE/src/com/jdbc/type5/Type3Test.java
@@ -0,0 +1,10 @@
+package com.jdbc.type5;
+
+public class Type3Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java SE/src/com/jdbc/type5/Type5Test.java
@@ -0,0 +1,10 @@
+package com.jdbc.type5;
+
+public class Type5Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
